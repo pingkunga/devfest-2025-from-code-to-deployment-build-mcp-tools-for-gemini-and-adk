9b9855d724e871d543d576e30e5161b6f06d50c9
Karn Wong <karn@karnwong.me>
2025-10-28 13:05:04 +0700

    init fastapi

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 # main.py
-from app.factory import create_app
-
-app = create_app()
-
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.app:app", host="0.0.0.0", port=8000, reload=True)
