8e399b71d5727061c1c15ec145cf2feda7271d3e
Witthawin <61728000+ro-witthawin@users.noreply.github.com>
2025-10-31 15:06:25 +0000

    Edit config file & Installation

--- .devcontainer/install-tools.sh
+++ .devcontainer/install-tools.sh
@@ -8,5 +8,7 @@ echo "Installing hurl..."
 VERSION=6.0.0
 curl --location --remote-name https://github.com/Orange-OpenSource/hurl/releases/download/$VERSION/hurl_${VERSION}_amd64.deb
 sudo apt update && sudo apt install ./hurl_${VERSION}_amd64.deb
+pip install fastmcp
+cp .env.example .env
 
 echo "All tools installed successfully!"
--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
-start:
+start-wehbook:
 	uv run main.py
-
+start-mcp-server:
+	python src/mcp_server/tools.py
 start-agent-a:
 	uv run adk api_server src/agent_a
 submit-agent-a:
